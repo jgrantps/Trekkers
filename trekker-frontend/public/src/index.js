44d2ed32dfdeea534548543088b96f33823eb73f
jgrantps <jgrantps@hotmail.com>
2020-01-26 17:15:56 -0400

    added tailwind utility functionality!!!

---
+++ trekker-frontend/public/src/index.js
@@ -0,0 +1,14 @@
+console.log('hello world')
+
+const BASE_URL = "http://localhost:3000"
+//..
+const USERS_URL = `${BASE_URL}+/users`
+const NEW_USER_URL = `${BASE_URL}+/users`
+const USER_URL = `${BASE_URL}+/users/${USER_ID}`
+//..
+const CHARACTERS_URL = `${BASE_URL}+/characters`
+const NEW_CHARACTER_URL = `${BASE_URL}+/characters`
+const CHARACTER_URL = `${BASE_URL}+/characters/${CHARACTER_ID}`
+const CHARACTERS_URL = `${BASE_URL}+/characters`
+//..
+const KITS_URL = `${BASE_URL}+/kits`
